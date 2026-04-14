946eeba9d07bfa7391f086140f8328beaf335933
alina.kudusova <.kudusova@localhost>
2026-04-14 19:26:29 +0300

    feat: configure OTEL parameters

--- build-docker/run.sh
+++ build-docker/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "Entrypoint sh script started"
-exec java -cp "app:app/lib/*" ${JAVA_OPTS} ${APPLICATIONCLASS} ${@}
+exec java -cp "app:app/lib/*" -javaagent:/app/opentelemetry-javaagent.jar ${JAVA_OPTS} ${APPLICATIONCLASS} ${@}
